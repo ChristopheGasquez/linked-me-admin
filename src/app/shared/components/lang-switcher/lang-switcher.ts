a804893cd9a84b75e67e98ce032201471f1469fa
gasquez.christophe <c.gasquez.dl@gmail.com>
2026-02-23 23:58:23 +0100

    feat: use transloco for lang switcher labels

--- src/app/shared/components/lang-switcher/lang-switcher.ts
+++ src/app/shared/components/lang-switcher/lang-switcher.ts
@@ -3,15 +3,13 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
-import { TranslocoService } from '@jsverse/transloco';
+import { TranslocoPipe, TranslocoService } from '@jsverse/transloco';
 
 import { AVAILABLE_LANGS } from '../../models/lang.model';
 
-const LANG_LABELS: Record<string, string> = { fr: 'Français', en: 'English' };
-
 @Component({
   selector: 'app-lang-switcher',
-  imports: [MatIconButton, MatIcon, MatMenu, MatMenuItem, MatMenuTrigger],
+  imports: [MatIconButton, MatIcon, MatMenu, MatMenuItem, MatMenuTrigger, TranslocoPipe],
   template: `
     <button mat-icon-button [matMenuTriggerFor]="langMenu">
       <mat-icon>language</mat-icon>
@@ -20,7 +18,7 @@ const LANG_LABELS: Record<string, string> = { fr: 'Français', en: 'English' };
       @for (lang of langs; track lang) {
         <button mat-menu-item (click)="setLang(lang)">
           <mat-icon>{{ activeLang() === lang ? 'check' : '' }}</mat-icon>
-          {{ langLabels[lang] }}
+          {{ 'common.langs.' + lang | transloco }}
         </button>
       }
     </mat-menu>
@@ -30,7 +28,6 @@ export class LangSwitcher {
   #transloco = inject(TranslocoService);
 
   langs = AVAILABLE_LANGS;
-  langLabels = LANG_LABELS;
   activeLang = toSignal(this.#transloco.langChanges$, {
     initialValue: this.#transloco.getActiveLang(),
   });
